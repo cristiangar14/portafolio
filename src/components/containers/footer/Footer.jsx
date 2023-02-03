fa572b4cb2c77a3630ecef655ed8c7348b776375
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-02 20:56:42 -0500

    Iniciando con el footer

--- src/components/containers/footer/Footer.jsx
+++ src/components/containers/footer/Footer.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Col, Container, Row } from 'reactstrap';
 
 import './footer.scss';
 
 const Footer = () => {
     return (
       <div className="footer">
-        footer
+        <Container fluid="sm">
+          <Row sm="2" xs="1">
+            <Col>
+              <div className="footer--left">
+                <div className="">
+                  Descaragr
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="footer--right">
+                <div className="">
+                  contac
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="footer--copy">
+              <p>Todos los derechos reservados</p>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
 };
