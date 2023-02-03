710dc8f032e6cd700c30279ce7713e588c609f50
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-03 14:21:56 -0500

    agregando mocks para datos de prueba

---
+++ src/mocks/about.mocks.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/prefer-default-export
+export const textsAbout = [
+    {
+        id: 1,
+        text: 'Soy un apasionado desarrollador frontend colombiano con sede en la vibrante ciudad de Ibagué, Colombia, con más de 3 años de experiencia en la construcción de aplicaciones web de alta calidad. Mi fortaleza radica en el lenguaje JavaScript, consumo de API REST, enfocado en la experiencia de usuario y en CLEAN CODE, lo que me permite trabajar con agilidad y eficacia en los proyectos que me encomiendan. Como autodidacta, me esfuerzo por mantenerme actualizado y mejorar constantemente mis habilidades. He tenido la oportunidad de manejar diferentes frameworks y librerías, como AngularJS y Angular 13, y trabajar con metodologías ágiles.',
+    },
+    {
+        id: 2,
+        text: 'Además, tengo conocimientos básicos en ReactJS, TypeScript, SASS y diferentes frameworks de CSS como Bootstrap y Materialize. También tengo experiencia en el uso de herramientas low code como WordPress y Apex Oracle en mantenimiento de páginas y proyectos empresariales.',
+    },
+    {
+        id: 3,
+        text: 'He trabajado de forma remota para equipos en diferentes ciudades y culturas en la industria IT, lo que me ha permitido desarrollar habilidades en la colaboración y el trabajo en equipo. Estoy disponible para trabajar en proyectos remotos y tengo la capacidad de colaborar en un entorno multicultural. He participado en mantenimiento de diferentes páginas web y en proyectos empresariales, incluyendo un software de gestión del consumo de gas natural de grandes industrias y un software contable utilizado por más de 1000 pequeñas y medianas empresas en Colombia.',
+    },
+    {
+        id: 3,
+        text: 'Sigo comprometido a crecer en mi carrera y aplicar mis conocimientos para lograr resultados sobresalientes en proyectos interesantes y desafiantes. Manejo eficientemente el control de versiones con Git y tengo como objetivo aprender inglés y convertirme en un desarrollador full-stack para poder trabajar en proyectos de mayor envergadura.',
+    },
+];
