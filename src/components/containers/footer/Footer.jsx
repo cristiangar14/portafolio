2df34c1a2df6d4881f8a0dc41c6b893d108388d7
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-02 23:30:51 -0500

    ajustando estilos del footer

--- src/components/containers/footer/Footer.jsx
+++ src/components/containers/footer/Footer.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import { Container } from 'reactstrap';
+import { Phone, Envelope } from 'react-bootstrap-icons';
 
 import './footer.scss';
 
 const Footer = () => {
     return (
       <div className="footer">
-        <Container fluid="sm">
-          <Row sm="2" xs="1">
-            <Col>
-              <div className="footer--left">
-                <div className="">
-                  Descaragr
-                </div>
-              </div>
-            </Col>
-            <Col>
-              <div className="footer--right">
-                <div className="">
-                  contac
-                </div>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="footer--copy">
-              <p>Todos los derechos reservados</p>
-            </Col>
-          </Row>
+        <Container fluid className="footer--container">
+          <div className="footer--left">
+            <div className="">
+              <a href="/">
+                <h2>Descarga mi CV</h2>
+              </a>
+            </div>
+          </div>
+          <div className="footer--right">
+            <div className="">
+              <h2>Contactame</h2>
+              {/* TODO agregar archivo apra descarga */}
+              <a href="documento.pdf" download="mi-archivo.pdf">
+                <Phone size={30} />
+                <p>+57 3178356463</p>
+              </a>
+              <a href="mailto:desarrollo.cristian.garzon@gmail.com">
+                <Envelope size={30} />
+                <p>desarrollo.cristian.garzon@gmail.com</p>
+              </a>
+            </div>
+          </div>
+          <div className="footer--copy">
+            <p>Todos los derechos reservados</p>
+          </div>
         </Container>
       </div>
     );
