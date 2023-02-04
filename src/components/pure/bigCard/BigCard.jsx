39aff8a16c75985eaf5a487a2ca5314abcf4598b
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-04 12:42:37 -0500

    componente card de experiencia laboral

---
+++ src/components/pure/bigCard/BigCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './bigCard.scss';
+
+const BigCard = ({ data }) => {
+    return (
+      <div className="bigCard">
+        <div className="bigCard--image">
+          <img src={data.logo} alt="compa" />
+        </div>
+        <div className="bigCard--content">
+          <h3>{data.jobTitle}</h3>
+          <h4>{data.company}</h4>
+          <p>{data.summary}</p>
+          <p>{data.dates}</p>
+        </div>
+        <div className="bigCard--link">
+          <Link to="/">Ver mas...</Link>
+        </div>
+      </div>
+    );
+};
+
+export default BigCard;
