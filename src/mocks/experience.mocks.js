35be728919c569bb24a1ccb52bed0fdd3a900753
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-04 12:41:42 -0500

    datos de experiancia laboral

---
+++ src/mocks/experience.mocks.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line import/prefer-default-export
+export const dataExperiences = [
+    {
+        id: 1,
+        logo: 'https://static.wixstatic.com/media/b52c48_098b66a09b1c40709f46f1ef44607858~mv2.jpg/v1/fit/w_2500,h_1330,al_c/b52c48_098b66a09b1c40709f46f1ef44607858~mv2.jpg',
+        company: 'Softpymes',
+        jobTitle: 'Desarrollador de software',
+        summary: 'Utilizando la metodolia Kanban-Scrum y herramientas como javascript, CSS, HTML, SQL, AngualrJS, Angular 13, DevExpress, git entre otras, dar manetniemiento al software comercializado por la compañia, asi mismo implementar nuevas funcionaldades propias de las reglas de negocio.',
+        companyDescription: 'Compañia dedicada al Desarrollo de software a la medida para pequeñas y grandes industrias.',
+        functions: 'Teniendo en cuenta la metodologia Kanban-Scrum dar mantenimiento y corregir bugs del software, para esto se usa herramientas como javascript, CSS, HTML, SQL, AngualrJS, Angular 13, DevExpress, control de veriones con git, tablero Kanban en Jira. De igual forma implementando nuevos productos para el software comercializado por la compañia. como por ejemplo una version beta de vistas de graficas entre otras.',
+        dates: '01/04/2022 / Actualmente',
+    },
+    {
+        id: 2,
+        logo: 'https://cdn.connectamericas.com/sites/default/files/styles/company_public_profile_picture/public/company-354163-1639174425.png?itok=cL7LruYI',
+        company: 'MBO Ingeniera de software',
+        jobTitle: 'Analista Funcional',
+        summary: 'Mediante la herramienta Oracle apex crear funcionalidades para diferente software empresarial, agregando reportes interactivos, descargas de documentos y en genral funcionaldiades propias del lado de negocio',
+        companyDescription: 'Compañia Oracle Partner, dedicada al Desarrollo de software a la medida.',
+        functions: 'Segun las reglas de nogocio implementar nuevas funcionalidades o modificar las existentes, utilizando herramientas como Oracle Apex con PL-SQL, javascript y CSS. Teniendo la oportunidad de participar enproyectos de dos grandes compañias en Colombia, en la implentacion de la automatización de proyectos mediante consultas automaticas, vistas, reportes y envios de emails, que mejoraron los tiempos de respuestas de las areas.',
+        dates: '15-11-2020 / 31-03/2022',
+    },
+    {
+        id: 3,
+        logo: 'https://scontent.fpei1-1.fna.fbcdn.net/v/t39.30808-6/277679701_406376621412797_1142574496299262749_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeFDId1lVTUg3ozes7mGVH8GlwQ28djVKoCXBDbx2NUqgK6X2WSvzz2X_5p_D2MOAYA&_nc_ohc=6mYQiQKZiC4AX8R53hG&_nc_ht=scontent.fpei1-1.fna&oh=00_AfAyqD9Y43mCGjcHIiVDL5HzCz-7ZmiUAp_Kn6cH2CrUQw&oe=63E2FF73',
+        jobTitle: 'Programador Web Junior',
+        company: 'Suwwweb',
+        summary: 'Crear y darle mantenimiento a direfentes paginas web, instalacion de SSL, creacion de correos corporativos, manejo del CPanel, direccionamiento de DNS...',
+        companyDescription: 'Agencia dedicada al Diseño Web, Estrategias Digitales y Posicionamiento',
+        functions: 'Crear paginas Web deacuerdo a los diseños y requerimientos entregados por la compañia, de igualforma asegurar la optimización de los mismo teniendo en cuenta legibilidad, peso de imagenes y calidad, Tambien dar mantenimiento a proyectos ya desarrollados en diferentes tecnologias como HTML, CSS, javascript, Wordpress, Joomla agregando o quitando funcionaldiades como por ejemplo impelmentar sliders, modificar banners, modificar datos de contacto, arreglar formularios de contancto, agregar servicios de google como recaptcha o google analitycs, Creación de correos corporativos en CPanel o Zoho, direccionamiento de DNS para correos o dominios',
+        dates: '15-07-2022 / Ocasionalmente',
+    },
+];
