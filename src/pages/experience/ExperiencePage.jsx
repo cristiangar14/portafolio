2ffdb3f3eb634911c3fd902a323011afe63dd7a6
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-04 12:45:56 -0500

    Vista de experiencia laboral

--- src/pages/experience/ExperiencePage.jsx
+++ src/pages/experience/ExperiencePage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Container } from 'reactstrap';
+import BigCard from '../../components/pure/bigCard/BigCard';
+import { dataExperiences } from '../../mocks/experience.mocks';
+
+import './experiencePage.scss';
 
 const ExperiencePage = () => {
     return (
-      <div>
-        Experiencia
-      </div>
+      <Container className="experience">
+        <h2>Experiencia IT</h2>
+        <div className="experience--conatainer">
+          {
+            dataExperiences.map((el) => (
+              <BigCard key={`experience-${el.id}`} data={el} />
+            ))
+          }
+        </div>
+      </Container>
     );
 };
 
