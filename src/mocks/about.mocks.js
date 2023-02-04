ba20e0a161c72bac47edc4bc0fe48c9409fe6f96
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-03 19:37:23 -0500

    Agregando textos y skills en vista acerca

--- src/mocks/about.mocks.js
+++ src/mocks/about.mocks.js
@@ -13,7 +13,62 @@ export const textsAbout = [
         text: 'He trabajado de forma remota para equipos en diferentes ciudades y culturas en la industria IT, lo que me ha permitido desarrollar habilidades en la colaboración y el trabajo en equipo. Estoy disponible para trabajar en proyectos remotos y tengo la capacidad de colaborar en un entorno multicultural. He participado en mantenimiento de diferentes páginas web y en proyectos empresariales, incluyendo un software de gestión del consumo de gas natural de grandes industrias y un software contable utilizado por más de 1000 pequeñas y medianas empresas en Colombia.',
     },
     {
-        id: 3,
+        id: 4,
         text: 'Sigo comprometido a crecer en mi carrera y aplicar mis conocimientos para lograr resultados sobresalientes en proyectos interesantes y desafiantes. Manejo eficientemente el control de versiones con Git y tengo como objetivo aprender inglés y convertirme en un desarrollador full-stack para poder trabajar en proyectos de mayor envergadura.',
+    },
+    {
+        id: 5,
+        text: 'Si desea conocer más sobre mi experiencia y portafolio, por favor visite los respectivos menús en mi perfil.',
+    },
+];
+
+export const skillsData = [
+    {
+        id: 1,
+        title: 'Tridente Web',
+        logo: 'https://cdn.worldvectorlogo.com/logos/logo-javascript.svg',
+        description: 'Solidas habilidades en HTML5, CSS3 y Javascript es+6, maquetación responsiva, consumo de APIs, manejo de eventos entre otros.',
+    },
+    {
+        id: 2,
+        title: 'Git y Github',
+        logo: 'https://cdn.worldvectorlogo.com/logos/github-icon.svg',
+        description: 'Manejo Git y Github para el correcto versionamiento del proyecto.',
+    },
+    {
+        id: 3,
+        title: 'Angular',
+        logo: 'https://cdn.worldvectorlogo.com/logos/angular-icon-1.svg',
+        description: 'He tenido la oportunidad de participar en proyectos que manejan AngularJS, Angular2 y Angular 13, lo cual me permitio conocer buena parte del funcionamiento de estos frameworks',
+    },
+    {
+        id: 4,
+        title: 'React Js',
+        logo: 'https://cdn.worldvectorlogo.com/logos/react-2.svg',
+        description: 'He realizado cursos en diversas plataformas que me permiten tener conocimientos de esta herramienta, como lo son las rutas, componentes funcionales, los hooks, y props entre otras caracteristicas que ofrece.',
+    },
+    {
+        id: 5,
+        title: 'SQL y SQL-Oracle',
+        logo: 'https://cdn.worldvectorlogo.com/logos/oracle-corporation-logo.svg',
+        description: 'Conocimiento tipos de datos relacionales, asi como consultas relacionadas con el CRUD',
+    },
+    {
+        id: 6,
+        title: 'Typescript',
+        logo: 'https://cdn.worldvectorlogo.com/logos/typescript.svg',
+        description: 'Conocimiento de la filosofia y conceptos basicos.',
+    },
+    {
+        id: 7,
+        title: 'Metodologias Agiles',
+        logo: 'https://cdn.worldvectorlogo.com/logos/scrum-1.svg',
+        description: 'En mi experiencia laboral he participado en proyectos que implementan metodologias Agiles, tales como Kanban, ademas de esto he tomado pequeños cursos para enteder SCRUM y Programación extrema',
+    },
+    {
+        id: 8,
+        title: 'Varios',
+        logo: 'https://img.freepik.com/vector-gratis/icono-programador-plano_1284-4642.jpg?w=740&t=st=1675465573~exp=1675466173~hmac=e1b926de214b25401995e756c723470fddefea0d2258fc22e2069f9ef88e8bf4',
+        description: 'Docker, Sass, Cpanel, instalacion de SSL, Wordpress',
     },
 ];
