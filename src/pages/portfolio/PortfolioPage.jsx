0f1af6f9decc4c5f65bb267fb791c9e19653cac1
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-06 22:44:19 -0500

    Iniciando con la vista portafolio

--- src/pages/portfolio/PortfolioPage.jsx
+++ src/pages/portfolio/PortfolioPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 
+import './portfolioPage.scss';
+
 const PortfolioPage = () => {
     return (
-      <Container>
-        Portafolioknsx.flkjbnasdjfbfjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+      <Container className="portfolio">
+        <h2>Portafolio</h2>
+        <div className="">
+          <h3>Profesional</h3>
+        </div>
+        <div className="">
+          <h3>Educativo</h3>
+        </div>
       </Container>
     );
 };
