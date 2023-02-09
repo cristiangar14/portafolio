c783de52d2b2f56153fe63f31144b69429174350
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-08 22:27:30 -0500

    ajustando card

--- src/components/pure/cardItem/CardItem.jsx
+++ src/components/pure/cardItem/CardItem.jsx
@@ -5,15 +5,15 @@ import './cardItem.scss';
 
 const CardItem = ({ data }) => {
     return (
-      <Link to="/" className="smallCard">
+      <Link to={`/portfolio/${data.id}`} className="smallCard">
         <div className="smallCard--container">
           <div className="smallCard--image">
             <img src="https://picsum.photos/800/600?random=1" alt="reandom" />
           </div>
           <div className="smallCard--content">
             <h4>{data.title}</h4>
-            <h5>Card subtitle</h5>
-            <p>texto jdsbfj jsdaafjkasbdf jbsdfkbasdf jsdbfkjasnfdkmas df ndsjfbsdjafb</p>
+            <h5>{data.subtitle}</h5>
+            <p>{data.shortDescription}</p>
             <p className="smallCard--details">Ver mas...</p>
           </div>
         </div>
