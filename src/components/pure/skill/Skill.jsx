54bb74b8e4e1272fda9f4ab205a190d4c6c66f83
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-09 23:44:08 -0500

    ajustando compnente de skills

--- src/components/pure/skill/Skill.jsx
+++ src/components/pure/skill/Skill.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import { Tooltip } from '@mui/material';
+import React, { forwardRef } from 'react';
 
 import './skill.scss';
 
 const Skill = ({ data }) => {
     const target = `skill${data.id}`;
 
+    // eslint-disable-next-line react/no-unstable-nested-components
+    const Skl = forwardRef((props, ref) => {
+      //  Spread the props to the underlying DOM element.
+      return (
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        <div {...props} ref={ref}>
+          <img className="skill--logo" src={data.logo} alt={data.title} />
+          <p id={target}>
+            {data.title}
+          </p>
+        </div>
+      );
+    });
+
   return (
-    <div className="skill">
-      <img className="skill--logo" src={data.logo} alt={data.title} />
-      <p id={target}>
-        {data.title}
-      </p>
-      <div>
-        {data.description}
-      </div>
-    </div>
+    <Tooltip title={data.description} className="skill" arrow placement="top-start">
+      <Skl />
+    </Tooltip>
   );
 };
 
