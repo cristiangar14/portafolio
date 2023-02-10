fe7102c8a18c70fd31352303f54547fec39dde16
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-09 23:24:12 -0500

    Ajuste de estilos home

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,7 +5,7 @@ import './homePage.scss';
 
 const HomePage = () => {
     return (
-      <Container id="home" className="home">
+      <Container className="home">
         <div className="">
           <h1 className="home--name">Cristian Garzón Sanchez</h1>
           <h2 className="home--job">Software Developer</h2>
