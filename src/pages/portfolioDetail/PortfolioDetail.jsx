ff53008db181a1b65d66be4c3be7d1aa0c8d248e
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-11 12:25:08 -0500

    Creando grid, ajustando estilos y añadiendo slider

--- src/pages/portfolioDetail/PortfolioDetail.jsx
+++ src/pages/portfolioDetail/PortfolioDetail.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import { Container } from '@mui/material';
+import React, { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import Linksproyects from '../../components/containers/linksProyects/Linksproyects';
+import Slider from '../../components/containers/slider/Slider';
+import Tecnologies from '../../components/containers/technologies/Technologies';
+import { getPortfolioDetail } from '../../services/data';
+
+import './portfolioDetail.scss';
 
 const PortfolioDetail = () => {
+  const params = useParams();
+
+    const data = useMemo(() => (
+      getPortfolioDetail(params.id)
+    ), [params.id]);
+
     return (
-      <div>
-        <p>portafolio detail</p>
+      <div className="portfolioDetails">
+        <div className="portfolioDetails--slider">
+          <Slider data={data.images} />
+        </div>
+        <Container>
+          <div className="portfolioDetails--title"><h2>{data.title}</h2></div>
+          <div className="portfolioDetails--container">
+            <div className="portfolioDetails--description">
+              <p>{data.description}</p>
+            </div>
+            <div className="portfolioDetails--tecnologies">
+              <Tecnologies technologies={data.technologies} />
+            </div>
+            <div className="portfolioDetails--links">
+              {
+                data.links.length ? <Linksproyects links={data.links} /> : <p>Links privados</p>
+              }
+            </div>
+            <div className="portfolioDetails--bottom">5</div>
+          </div>
+        </Container>
       </div>
     );
 };
