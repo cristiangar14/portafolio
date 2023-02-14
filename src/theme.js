d9247ef9c5985162fb43081971881dd20e19d31c
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-13 23:51:31 -0500

    Creando tema para formulario

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#61DAFB',
+    },
+    secondary: {
+      main: '#20232A',
+    },
+  },
+});
+
+export default theme;
