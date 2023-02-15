9332770c02ac767e447dcf1db59437e2a98902d8
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-14 20:27:20 -0500

    agregando datos de recursos

---
+++ src/mocks/resources.mocks.js
@@ -0,0 +1,59 @@
+// eslint-disable-next-line import/prefer-default-export
+export const resourcesData = [
+  {
+    id: 1,
+    category: 'Organizanción',
+    title: 'Notion',
+    url: 'https://www.notion.so/es-es',
+    description: 'Crear Kanban, agendas, notas',
+  },
+  {
+    id: 2,
+    category: 'desarrollo',
+    title: 'AngularJS',
+    url: 'https://docs.angularjs.org/api/ng/directive/ngModel',
+    description: 'documentación oficial de AngularJS',
+  },
+  {
+    id: 3,
+    category: 'desarrollo',
+    title: 'AngularJS',
+    url: 'https://docs.angularjs.org/api/ng/directive/ngModel',
+    description: 'documentación oficial de AngularJS',
+  },
+  {
+    id: 4,
+    category: 'Diseño',
+    title: 'onlineconvertfree',
+    url: 'https://onlineconvertfree.com/es/convert/jpeg/',
+    description: 'Convertir imagenes a webp y diferentes formatos',
+  },
+  {
+    id: 5,
+    category: 'Diseño',
+    title: 'worldvectorlog',
+    url: 'https://worldvectorlogo.com/',
+    description: 'Biblioteca de logos en svg gratis',
+  },
+  {
+    id: 6,
+    category: 'Diseño',
+    title: 'favicon.io',
+    url: 'https://favicon.io/favicon-generator/',
+    description: 'Generador de Favicons gratis',
+  },
+  {
+    id: 7,
+    category: 'Diseño',
+    title: 'svg porn',
+    url: 'https://svgporn.com/',
+    description: 'Biblioteca de logos en svg gratis',
+  },
+  {
+    id: 8,
+    category: 'Desarrollo',
+    title: 'roadmap',
+    url: 'https://roadmap.sh/',
+    description: 'Rutas de aprendizaje de desarrollo',
+  },
+];
