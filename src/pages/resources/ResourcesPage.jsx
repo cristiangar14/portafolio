9144a5fe70fbc0d542e5fb1d7ca8b2fd66258b8a
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-14 20:30:04 -0500

    Creando componente de recursos

--- src/pages/resources/ResourcesPage.jsx
+++ src/pages/resources/ResourcesPage.jsx
@@ -1,10 +1,22 @@
+import { Container } from '@mui/material';
 import React from 'react';
+import SmallCard from '../../components/pure/cardSource/SmallCard';
+
+import { resourcesData } from '../../mocks/resources.mocks';
+import './resourcePage.scss';
 
 const ResourcesPage = () => {
     return (
-      <div>
-        Recursos
-      </div>
+      <Container className="resources">
+        <h1>Recursos</h1>
+        <div className="resources--container">
+          {
+            resourcesData.map((el) => {
+              return <SmallCard key={`sources-${el.id}`} data={el} />;
+            })
+          }
+        </div>
+      </Container>
     );
 };
 
