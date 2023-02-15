276d054d9f6eff868a82fc78c5ae426865846b47
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-14 20:31:39 -0500

    Ajustando estilos

--- src/components/pure/cardItem/CardItem.jsx
+++ src/components/pure/cardItem/CardItem.jsx
@@ -5,16 +5,16 @@ import './cardItem.scss';
 
 const CardItem = ({ data }) => {
     return (
-      <Link to={`/portfolio/${data.id}`} className="smallCard">
-        <div className="smallCard--container">
-          <div className="smallCard--image">
+      <Link to={`/portfolio/${data.id}`} className="Card">
+        <div className="Card--container">
+          <div className="Card--image">
             <img src={data.shortImage.url} alt={data.shortImage.alt} />
           </div>
-          <div className="smallCard--content">
+          <div className="Card--content">
             <h4>{data.title}</h4>
             <h5>{data.category}</h5>
             <p>{data.shortDescription}</p>
-            <p className="smallCard--details">Ver mas...</p>
+            <p className="Card--details">Ver mas...</p>
           </div>
         </div>
       </Link>
