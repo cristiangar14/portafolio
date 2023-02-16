3726ed0ee55488a93ba3d59ad3529815105625ee
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-15 20:34:18 -0500

    ajustando nombres de clases

--- src/components/pure/cardItem/CardItem.jsx
+++ src/components/pure/cardItem/CardItem.jsx
@@ -5,16 +5,16 @@ import './cardItem.scss';
 
 const CardItem = ({ data }) => {
     return (
-      <Link to={`/portfolio/${data.id}`} className="Card">
-        <div className="Card--container">
-          <div className="Card--image">
+      <Link to={`/portfolio/${data.id}`} className="cardItem">
+        <div className="cardItem--container">
+          <div className="cardItem--image">
             <img src={data.shortImage.url} alt={data.shortImage.alt} />
           </div>
-          <div className="Card--content">
+          <div className="cardItem--content">
             <h4>{data.title}</h4>
             <h5>{data.category}</h5>
             <p>{data.shortDescription}</p>
-            <p className="Card--details">Ver mas...</p>
+            <p className="cardItem--details">Ver mas...</p>
           </div>
         </div>
       </Link>
