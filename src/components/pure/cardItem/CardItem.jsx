9706bb4e8d48dcfa0744c8e5f15f01188971ea57
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-16 22:07:28 -0500

    ajustando estilos de las cards

--- src/components/pure/cardItem/CardItem.jsx
+++ src/components/pure/cardItem/CardItem.jsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardMedia } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -6,17 +7,25 @@ import './cardItem.scss';
 const CardItem = ({ data }) => {
     return (
       <Link to={`/portfolio/${data.id}`} className="cardItem">
-        <div className="cardItem--container">
-          <div className="cardItem--image">
-            <img src={data.shortImage.url} alt={data.shortImage.alt} />
-          </div>
-          <div className="cardItem--content">
-            <h4>{data.title}</h4>
-            <h5>{data.category}</h5>
-            <p>{data.shortDescription}</p>
-            <p className="cardItem--details">Ver mas...</p>
-          </div>
-        </div>
+        <Card sx={{ maxWidth: 345 }} className="cardItem--container">
+          {/* <div className="cardItem--image"> */}
+          <CardMedia
+            sx={{ height: 180 }}
+            image={data.shortImage.url}
+            title={data.shortImage.alt}
+          />
+          {/* </div> */}
+          <CardContent className="cardItem--content">
+            <div className="">
+              <h4>{data.title}</h4>
+              <h5>{data.category}</h5>
+              <p>{data.shortDescription}</p>
+            </div>
+            <div className="">
+              <p className="cardItem--details">Ver mas...</p>
+            </div>
+          </CardContent>
+        </Card>
       </Link>
 
     );
