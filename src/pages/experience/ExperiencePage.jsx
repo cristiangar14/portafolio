b2c8c18fde6ee4fbe9b272ab59e3c7ac55f6cd40
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-20 21:00:41 -0500

    Conectando experiences con la db

--- src/pages/experience/ExperiencePage.jsx
+++ src/pages/experience/ExperiencePage.jsx
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ref, onValue } from 'firebase/database';
+import {
+  CircularProgress,
+  Container,
+  ThemeProvider,
+} from '@mui/material';
 import BigCard from '../../components/pure/bigCard/BigCard';
-import { dataExperiences } from '../../mocks/experience.mocks';
-
+import db from '../../../firebase';
+import theme from '../../theme';
 import './experiencePage.scss';
 
+const initialStateLoading = {
+  experiencesLoading: false,
+};
+
 const ExperiencePage = () => {
+  const [loading, setLoading] = useState(initialStateLoading);
+  const [dataExperiences, setDataExperiences] = useState([]);
+
+    useEffect(() => {
+      setLoading({ experiencesLoading: true });
+    const startAboutText = ref(db, '/experience');
+    onValue(startAboutText, (snapshot) => {
+        const data = snapshot.val();
+        setDataExperiences(data);
+        setLoading({ experiencesLoading: false });
+      });
+    }, []);
+
     return (
-      <div className="experience">
+      <Container className="experience">
         <h2>Experiencia IT</h2>
-        <div className="experience--conatainer">
-          {
+        {
+          loading.experiencesLoading
+          ? (
+            <ThemeProvider theme={theme}>
+              <CircularProgress />
+            </ThemeProvider>
+          )
+          : (
+            <div className="experience--conatainer">
+              {
             dataExperiences.map((el) => (
               <BigCard key={`experience-${el.id}-${el.company}`} data={el} />
             ))
           }
-        </div>
-      </div>
+            </div>
+          )
+
+        }
+      </Container>
     );
 };
 
