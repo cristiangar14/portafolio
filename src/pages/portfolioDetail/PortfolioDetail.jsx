0a99f4ddf65568fdd6cf4c48e71fe99f3bd12b11
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-21 21:02:48 -0500

    implementando firebase en portfolioDetails

--- src/pages/portfolioDetail/PortfolioDetail.jsx
+++ src/pages/portfolioDetail/PortfolioDetail.jsx
@@ -1,28 +1,44 @@
 import { Button, Container, ThemeProvider } from '@mui/material';
-import React, { useMemo } from 'react';
+import { onValue, ref } from 'firebase/database';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import db from '../../../firebase';
 import Linksproyects from '../../components/containers/linksProyects/Linksproyects';
 import Slider from '../../components/containers/slider/Slider';
 import Tecnologies from '../../components/containers/technologies/Technologies';
-import { getPortfolioDetail } from '../../services/data';
+import Loader from '../../components/pure/loader/Loader';
 import theme from '../../theme';
 
 import './portfolioDetail.scss';
 
+const initialStateLoading = {
+  dataLoading: true,
+};
 const PortfolioDetail = () => {
   const params = useParams();
 
-    const data = useMemo(() => (
-      getPortfolioDetail(params.id)
-    ), [params.id]);
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(initialStateLoading);
+
+      useEffect(() => {
+        setLoading({ dataLoading: true });
+        const startAboutText = ref(db, `/portfolio/${params.id}`);
+        onValue(startAboutText, (snapshot) => {
+            setData(snapshot.val());
+            setLoading({ dataLoading: false });
+          });
+      }, []);
 
     const navigate = useNavigate();
-
     const handleNavigation = () => {
       navigate('/portfolio');
     };
 
-    return (
+    return loading.dataLoading && !data.title
+    ? (
+      <Loader />
+    )
+    : (
       <div className="portfolioDetails">
         <div className="portfolioDetails--slider">
           <Slider data={data.images} />
