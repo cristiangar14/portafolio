d049a441204f4fcfdc2d9d28df2cc587f0a7d2ea
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-02-21 21:09:59 -0500

    implementado en firestore

--- src/pages/portfolio/PortfolioPage.jsx
+++ src/pages/portfolio/PortfolioPage.jsx
@@ -1,21 +1,34 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Container } from '@mui/system';
+import { onValue, ref } from 'firebase/database';
 import CardItem from '../../components/pure/cardItem/CardItem';
-import { getPortfolio } from '../../services/data';
 
 import './portfolioPage.scss';
+import db from '../../../firebase';
+import Loader from '../../components/pure/loader/Loader';
 
+const initialStateLoading = {
+  dataLoading: true,
+};
 const PortfolioPage = () => {
-  const data = useMemo(() => (
-        getPortfolio()
-  ), []);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(initialStateLoading);
 
-  // const dataE = useMemo(() => (
-  //       getPortfolio('E')
-  // ), []);
+    useEffect(() => {
+      setLoading({ dataLoading: true });
+      const startAboutText = ref(db, '/portfolio');
+      onValue(startAboutText, (snapshot) => {
+          setData(snapshot.val());
+          setLoading({ dataLoading: false });
+        });
+    }, []);
 
-    return (
+    return loading.dataLoading
+    ? (
+      <Loader />
+    )
+    : (
       <Container className="portfolio">
         <h2>Portafolio</h2>
         <ul className="portfolio--list">
