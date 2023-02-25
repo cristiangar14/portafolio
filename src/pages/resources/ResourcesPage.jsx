47a001f6bc14232d0ca36d5d1aaec575cdfdb68e
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-02-25 00:54:30 -0500

    Conectando sources con firebase

--- src/pages/resources/ResourcesPage.jsx
+++ src/pages/resources/ResourcesPage.jsx
@@ -1,17 +1,41 @@
 import { Container } from '@mui/material';
-import React from 'react';
+import { onValue, ref } from 'firebase/database';
+import React, { useEffect, useState } from 'react';
+import db from '../../../firebase';
 import SmallCard from '../../components/pure/cardSource/SmallCard';
+import Loader from '../../components/pure/loader/Loader';
 
-import { resourcesData } from '../../mocks/resources.mocks';
 import './resourcePage.scss';
 
+const initialStateLoading = {
+  dataLoading: true,
+};
+
 const ResourcesPage = () => {
-    return (
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(initialStateLoading);
+
+    useEffect(() => {
+      setLoading({ dataLoading: true });
+      const startAboutText = ref(db, '/resources');
+      onValue(startAboutText, (snapshot) => {
+          setData(snapshot.val());
+          setLoading({ dataLoading: false });
+        }, {
+          onlyOnce: true,
+        });
+    }, []);
+
+    return loading.dataLoading
+    ? (
+      <Loader />
+    )
+    : (
       <Container className="resources">
         <h1>Recursos</h1>
         <div className="resources--container">
           {
-            resourcesData.map((el) => {
+            data.map((el) => {
               return <SmallCard key={`sources-${el.id}`} data={el} />;
             })
           }
