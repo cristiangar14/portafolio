1495f8ace5820cba3de336002d928c73fc3fa4d6
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-02-26 16:40:55 -0500

    Agregando pagina 404

--- src/pages/404/NotFoundPage.jsx
+++ src/pages/404/NotFoundPage.jsx
@@ -1,10 +1,14 @@
+import { Container } from '@mui/material';
 import React from 'react';
+
+import './notFoundPage.scss';
 
 const NotFoundPage = () => {
     return (
-      <div>
-        <p>NotFoundPage</p>
-      </div>
+      <Container className="notFound">
+        <h2>Not Found 404</h2>
+        <p>Contenido no encontrado, Por favor explora otro contenido de mi web.</p>
+      </Container>
     );
 };
 
