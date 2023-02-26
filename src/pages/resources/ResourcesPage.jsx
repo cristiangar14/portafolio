1b90406ad68ce395437e3a3952c7366a1615a30e
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-02-26 13:29:42 -0500

    Agregando buscador a recursos

--- src/pages/resources/ResourcesPage.jsx
+++ src/pages/resources/ResourcesPage.jsx
@@ -1,9 +1,10 @@
-import { Container } from '@mui/material';
+import { Container, TextField, ThemeProvider } from '@mui/material';
 import { onValue, ref } from 'firebase/database';
 import React, { useEffect, useState } from 'react';
 import db from '../../../firebase';
 import SmallCard from '../../components/pure/cardSource/SmallCard';
 import Loader from '../../components/pure/loader/Loader';
+import theme from '../../theme';
 
 import './resourcePage.scss';
 
@@ -13,7 +14,9 @@ const initialStateLoading = {
 
 const ResourcesPage = () => {
   const [data, setData] = useState([]);
+  const [dataSearch, setDataSearch] = useState([]);
   const [loading, setLoading] = useState(initialStateLoading);
+  const [inputSearch, setInputSearch] = useState('');
 
     useEffect(() => {
       setLoading({ dataLoading: true });
@@ -26,6 +29,14 @@ const ResourcesPage = () => {
         });
     }, []);
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const inpSearch = e.target.value.toLowerCase();
+    const search = data.filter(({ title, category, description }) => title.toLowerCase().includes(inpSearch) || category.toLowerCase().includes(inpSearch) || description.toLowerCase().includes(inpSearch));
+    setInputSearch(inpSearch);
+    setDataSearch(search);
+  };
+
     return loading.dataLoading
     ? (
       <Loader />
@@ -33,11 +44,44 @@ const ResourcesPage = () => {
     : (
       <Container className="resources">
         <h1>Recursos</h1>
+        <div className="resources--search">
+          <div className="resources--search__input">
+            <ThemeProvider theme={theme}>
+              <TextField
+                label="Buscar"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                onChange={handleSearch}
+                type="text"
+              />
+            </ThemeProvider>
+          </div>
+          {
+            inputSearch.length > 0 && (
+              dataSearch.length
+              ? (
+                <div className="resources--container resources--search__container">
+                  {
+                    dataSearch.map((el) => {
+                      return <SmallCard key={`sources-${el.id}`} data={el} />;
+                    })
+                  }
+                </div>
+              )
+              : (
+                <p className="resources--search__notFount">Sin resultados para su busqueda.</p>
+              )
+            )
+          }
+        </div>
         <div className="resources--container">
           {
-            data.map((el) => {
-              return <SmallCard key={`sources-${el.id}`} data={el} />;
-            })
+            inputSearch.length === 0 && (
+                data.map((el) => {
+                  return <SmallCard key={`sources-${el.id}`} data={el} />;
+                })
+                )
           }
         </div>
       </Container>
